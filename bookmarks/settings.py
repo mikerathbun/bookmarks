0f45d2f840c5255d4496ba1c622e3a899462069d
Mike Rathbun <mikerathbun@me.com>
2019-02-05 13:14:20 -0600

    added django authentication

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -114,7 +114,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
