9e6838733efc80ed61e6ee9c8bb6690e8d3d9c72
Mike Rathbun <mikerathbun@me.com>
2019-02-06 09:40:28 -0600

    Completed registration

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -113,7 +113,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
