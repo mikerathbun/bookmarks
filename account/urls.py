f3b6cf94922125f5952d6ec8c681600760e1cde4
Mike Rathbun <mikerathbun@me.com>
2019-02-11 21:31:51 +0000

    extended user model

--- account/urls.py
+++ account/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     #      auth_views.PasswordResetCompleteView.as_view(),
     #      name='password_reset_complete'),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
 ]
